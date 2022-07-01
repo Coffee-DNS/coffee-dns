98a1dc66f055c0d597798ef2233fcd5a2990f2be
jsirianni <joe.sirianni@bluemedora.com>
2022-06-30 23:45:21 -0400

    scan licenses

--- internal/tools/main.go
+++ internal/tools/main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/securego/gosec/v2"
 	_ "github.com/securego/gosec/v2/report/sarif" // required by gosec
 	_ "github.com/securego/gosec/v2/report/text"  // required by gosec
+	_ "github.com/uw-labs/lichen"
 	_ "honnef.co/go/tools/cmd/staticcheck"
 )
 
